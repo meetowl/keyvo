617193459324d6bdbaa04c6ebc3d6e4565a67f75
Michael <mishuk2000@gmail.com>
2019-05-17 13:30:12 +0100

    implemented backspace

--- main.c
+++ main.c
@@ -8,6 +8,8 @@
 #define WRONG_L COLOR_PAIR(1)
 #define WRONG_HL COLOR_PAIR(2)
 #define CORRECT_L COLOR_PAIR(3)
+#define REWRITE_L COLOR_PAIR(4)
+#define REWRITE_HL COLOR_PAIR(5)
 
 /* Test string size */ 
 #define TEST_SIZE_CHAR 321
@@ -16,12 +18,22 @@
 struct window{
 	WINDOW *window_p;
 	WINDOW *border_p;
-	int height, length;
+	int height;
+	int	length;
+	int start_curs_y;
+	int start_curs_x;
 };
 
 void exit_application(int stat){
 	endwin();
 	exit(stat);
+}
+
+int * get_std_height_length(){
+	int * return_size = malloc(3*sizeof(int));
+	getmaxyx(stdscr, *return_size, *(return_size+1));
+	*(return_size+2) = '\0';
+	return return_size;
 }
 
 void set_fonts(int theme){ // TODO: Allow different themes
@@ -30,32 +42,40 @@ void set_fonts(int theme){ // TODO: Allow different themes
 		init_pair(1, COLOR_BLACK, COLOR_WHITE); /* Wrong letter typed */
 		init_pair(2, COLOR_BLACK, COLOR_WHITE); /* Wrong space / newline */ 
 		init_pair(3, COLOR_WHITE, COLOR_BLACK); /* Correct letter typed */
+		init_pair(4, COLOR_WHITE, COLOR_WHITE); /* Backspaced to letter */
+		init_pair(5, COLOR_WHITE, COLOR_WHITE); /* Backspaced to whitespace */
 		break;
 	case 1:
 		init_pair(1, COLOR_RED, COLOR_BLACK); /* Wrong letter typed */
-		init_pair(2, COLOR_RED, COLOR_RED); /* Wrong space / newline */ 
+		init_pair(2, COLOR_RED, COLOR_RED); /* Wrong whitespace */ 
 		init_pair(3, COLOR_GREEN, COLOR_BLACK); /* Correct letter typed */
+		init_pair(4, COLOR_YELLOW, COLOR_BLACK); /* Backspaced to letter */
+		init_pair(5, COLOR_YELLOW, COLOR_YELLOW); /* Backspaced to whitespace */
 		break;
 	}
 }
+
 /* Creates the window in the given window structure pointer */
-void create_w(struct window *op_window, struct window *std_window){
-	
-	int win_start_h = (std_window->height/2) - (op_window->height/2);
-	int win_start_l = (std_window->length/2) - (op_window->length/2);
+void create_w(struct window *op_window){
+	int *std_dim = get_std_height_length();
+	int win_start_h = (*std_dim/2) - (op_window->height/2);
+	int win_start_l = (*(std_dim+1)/2) - (op_window->length/2);
 	op_window->window_p = newwin(op_window->height, op_window->length,	\
 								 win_start_h, win_start_l);
 }
+
 /* Creates the window border in the given window structure pointer */
-void create_w_brdr(struct window *op_window, struct window *std_window){
+void create_w_brdr(struct window *op_window){
+	int *std_dim = get_std_height_length();
+	
 	int border_h = op_window->height+3;
 	int border_l = op_window->length+3;
-	int border_start_h = ((std_window->height/2) - (op_window->height/2)) - 1;
-	int border_start_l = ((std_window->length/2) - (op_window->length/2)) - 1;
+	int border_start_h = *std_dim/2 - (op_window->height/2) - 1;
+	int border_start_l = *(std_dim+1)/2 - op_window->length/2 - 1;
 
-	op_window->border_p = newwin(border_h, border_l, border_start_h, border_start_l);
+	op_window->border_p = newwin(border_h, border_l,				\
+								 border_start_h, border_start_l);
 	wborder(op_window->border_p, '|', '|', '_', '_', ' ', ' ', ' ', ' ');
-
 }
 
 void start_curses(){
@@ -107,6 +127,8 @@ void print_option_menu(struct window *op_window){
 
 	WINDOW * win = op_window->window_p;
 	int win_l = op_window->length;
+	op_window->start_curs_y = 7;
+	op_window->start_curs_x = (win_l - strlen(line_7))/2 + strlen(line_7) + 1;
 	
 	mvwprintw(win, 0, (win_l - strlen(line_0))/2, "%s", line_0);
 	mvwprintw(win, 1, (win_l - strlen(line_1))/2, "%s", line_1);
@@ -125,24 +147,38 @@ void print_typing_string(struct window *op_window){
 
 	WINDOW * win = op_window->window_p;
 	int win_l=op_window->length;
+	op_window->start_curs_y = 4;
+	op_window->start_curs_x = 0;
 
 	mvwprintw(win, 0, (win_l - strlen(line_0))/2, "%s", line_0);
 	mvwprintw(win, 1, (win_l - strlen(line_1))/2, "%s", line_1);
 	mvwprintw(win, 2, (win_l - strlen(line_2))/2, "%s", line_2);
-
+	
 		
 		
 }
-void configure_window(struct window *op_window, 			\
-					  struct window *std_window,			\
-					  int wdw_h, int wdw_l){				
 
-	op_window->length = wdw_l;
+
+
+void configure_window(struct window *op_window){
+	int *std_dim = get_std_height_length();
+	int wdw_h = scaled_height(*std_dim),		\
+		wdw_l = scaled_length(*(std_dim+1));
+
+	// Makes sure screen is sufficient size
+	if(wdw_h == -1 || wdw_l == -1){
+		// TODO: write minimum x and y to resize to
+		printf("Please resize your terminal!\n");
+		exit_application(1);
+	}
+
+	// Set the struct's parameters
+	op_window ->length = wdw_l;
 	op_window->height = wdw_h;
-	create_w(op_window, std_window);
-	create_w_brdr(op_window, std_window);
-			 
+	create_w(op_window);
+	create_w_brdr(op_window);
 }
+
 char * generate_random_string(int gen_num){
 	const char char_array[] = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQq"	\
 		"RrSsTtUuWwXxYyZz";
@@ -201,89 +237,107 @@ void show_window(struct window *op_window){
 }
 
 
-void redraw_char(struct window *op_window, const char * test_char,	\
+void redraw_char(struct window *op_window, const char *test_char,	\
 				 const char in_correct){
-				 
-	if(in_correct){
+	switch(in_correct){
+	case 0:
 		waddch(op_window->window_p, *test_char | CORRECT_L);
-	}else if(*test_char == ' '){
-		waddch(op_window->window_p, *test_char | WRONG_HL);
-	}else{
-		waddch(op_window->window_p, *test_char | WRONG_L);
+		break;
+	case 1:
+		if(*test_char == ' '){
+			waddch(op_window->window_p, *test_char | WRONG_HL);
+		}else{
+			waddch(op_window->window_p, *test_char | WRONG_L);
+		}
+		break;
+	case 2:
+		if(*test_char == ' '){
+			waddch(op_window->window_p, *test_char | REWRITE_HL);		
+		}else{
+			waddch(op_window->window_p, *test_char | REWRITE_L);
+		}
+		break;
+	default:
+		fprintf(stderr,											\
+				"error: incorrect option (%d) passed to redraw_char().\n", \
+				in_correct);
+		exit_application(1);
+		break;
 	}
-		
-
-	wrefresh(op_window->window_p);
 }
-int is_char_correct(const char * test_char, char in_char){
-	if(in_char == *test_char){
-		return 1;
+
+// should return the timing array for analysis
+void test_string(char *str){
+	// Configure the window
+	struct window typing_window;
+	configure_window(&typing_window);
+	print_typing_string(&typing_window);
+	print_test_string(&typing_window, str);
+	show_window(&typing_window);
+	wmove(typing_window.window_p, 4, 0);
+
+	// Start testing keystrokes until we have reached
+	// the string's terminator char
+	int i = 0;
+	int e = 0;
+	while(*(str+i) != '\0'){
+		int char_in = getch();
+		// Backspace
+		if(char_in == 263){
+			int y, x;
+			// Macro that determines the position of cursor
+			getyx(typing_window.window_p, y, x);
+			if(x == 0){
+				if(y != typing_window.start_curs_y){
+					i--;
+					wmove(typing_window.window_p, y-1, typing_window.length-1);
+					redraw_char(&typing_window, str+i, 2);
+					wmove(typing_window.window_p, y-1, typing_window.length-1);
+				}
+			}else{
+				i--;
+				wmove(typing_window.window_p, y, x-1);
+				redraw_char(&typing_window, str+i, 2);
+				wmove(typing_window.window_p, y, x-1);
+			}
+		}else if(char_in == *(str+i)){
+			redraw_char(&typing_window, str+i, 0);
+			i++;
+		}else{
+			redraw_char(&typing_window, str+i, 1);
+			i++;
+		}
+		wrefresh(typing_window.window_p);
 	}
-	return 0;
 }
-
+				
 int main(){
 	start_curses();
 
-	struct window std_window;
-	getmaxyx(stdscr, std_window.height, std_window.length);
-	int wdw_h = scaled_height(std_window.height);
-	int wdw_l = scaled_length(std_window.length);
-	if( wdw_h == -1 || wdw_l == -1){
-		printf("Please resize your terminal!\n");
-		exit_application(1);
-	}
-	
 	struct window welcome_menu;
-	configure_window(&welcome_menu, &std_window, wdw_h, wdw_l);
-	//	print_option_menu(&welcome_menu);
-	struct window type_window;
-	configure_window(&type_window, &std_window, wdw_h, wdw_l);
-	//	print_typing_string(&type_window);
-	struct window result_window;
-	configure_window(&result_window, &std_window, wdw_h, wdw_l);
+	configure_window(&welcome_menu);
 
-
-
-
-	
+	// Start of what the typist interacts with 
+	// Quits when 'q' is pressed (char 65)
 	int option_slct = 0;
-	/* Start of what the typist interacts with 
-	   Quits when 'q' is pressed (char 65)
-	 */
-	do{
-		werase(type_window.window_p);
+	while(option_slct != 65){
+		// Show window, take input
 		print_option_menu(&welcome_menu);
 		show_window(&welcome_menu);
 		option_slct = getch() - '0';
-		static char * test_str;
-		if(option_slct == 1 || option_slct == 2 ||	\
-		   option_slct == 3){
-			test_str = generate_random_string(option_slct);
-			werase(welcome_menu.window_p);
-			print_typing_string(&type_window);
-			print_test_string(&type_window, test_str);
-			show_window(&type_window);
+
+		// Makes sure correct number is entered
+		while((option_slct > 3 ||				\
+			   option_slct < 1) &&				\
+			  option_slct != 65){
+			option_slct = getch() - '0';
 		}
 
-		if(option_slct != 65){
-			wmove(type_window.window_p, 4, 0);
-			int i = 0;
-			int time_new;
-			int *time_last = &time_new;
-			int *time_array = malloc(TEST_SIZE_CHAR*sizeof(int));
-			while(*(test_str+i) != '\0'){
-				int in_correct = is_char_correct(test_str+i, getch());
-				time_new = time(0);
-				*(time_array+i) = time_new - *time_last -1;
-				//				printw("%d, %d, %d",*(time_array+i), time_new, *time_last);
-				int temp = time_new;
-				*time_last =  &temp;
-				redraw_char(&type_window, test_str+i, in_correct);
-				i++;
-			}
-			int time_end = time(0);
+		if(option_slct > 0 && option_slct < 4){
+			char *test_str = generate_random_string(option_slct);
+			werase(welcome_menu.window_p);
+			test_string(test_str);
 		}
-	}while(option_slct != 65);
+	}
 	exit_application(0);
 }
