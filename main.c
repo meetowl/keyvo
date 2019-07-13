ece13a0a9cb91787aa11df8ff22f62f544a121cb
Michael <mishuk2000@gmail.com>
2019-07-13 19:18:40 +0100

    Statistics structure, timings and results window implemented.
    Statistics is now a struct instead of being an ungodly array of 3 different-byte-size arrays
    
    Timing now works using gettimeofday() which is much more accurate than previously proposed clock() function
    
    Results/statistics screen has been begun to be implemented and some basic statistics have been included (just their names, not implementation)

--- main.c
+++ main.c
@@ -3,7 +3,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ncurses.h>
-#include <time.h>
+#include <sys/time.h>
+
 
 #define WRONG_L COLOR_PAIR(1)
 #define WRONG_HL COLOR_PAIR(2)
@@ -12,7 +13,7 @@
 #define REWRITE_HL COLOR_PAIR(5)
 
 /* Test string size */ 
-#define TEST_SIZE_CHAR 321
+#define TEST_STR_SIZE 5
 
 /* Windows to be used */
 struct window{
@@ -22,6 +23,15 @@ struct window{
 	int	length;
 	int start_curs_y;
 	int start_curs_x;
+};
+
+struct keystat{
+	char *test_str;
+	int test_str_size;
+	char entered_str[TEST_STR_SIZE*4];
+	char char_input[TEST_STR_SIZE];
+	int timings[TEST_STR_SIZE];
+	int bs[TEST_STR_SIZE];
 };
 
 void exit_application(int stat){
@@ -142,10 +152,10 @@ void print_option_menu(struct window *op_window){
 
 void print_typing_string(struct window *op_window){
 	const char line_0[] = "Keyvo (by meetowl)";
-	const char line_1[] = "Type the characters, no backspaces.";
+	const char line_1[] = "Type the characters.";
 	const char line_2[] = "F1 to return to menu | F3 to regenerate string";
 
-	WINDOW * win = op_window->window_p;
+	WINDOW *win = op_window->window_p;
 	int win_l=op_window->length;
 	op_window->start_curs_y = 4;
 	op_window->start_curs_x = 0;
@@ -153,9 +163,6 @@ void print_typing_string(struct window *op_window){
 	mvwprintw(win, 0, (win_l - strlen(line_0))/2, "%s", line_0);
 	mvwprintw(win, 1, (win_l - strlen(line_1))/2, "%s", line_1);
 	mvwprintw(win, 2, (win_l - strlen(line_2))/2, "%s", line_2);
-	
-		
-		
 }
 
 
@@ -185,11 +192,13 @@ char * generate_random_string(int gen_num){
 	const char number_array[] = "1234567890";
 	const char symbol_array[] = "~`!@#$%^&*(){[}]+=?/_-:;\\|\"'";
 
-	srand(time(0));
+	struct timeval rand_tv;
+	gettimeofday(&rand_tv, 0);
+	srand(rand_tv.tv_sec+rand_tv.tv_usec);
 	
-	char *rdm_str = malloc(TEST_SIZE_CHAR*sizeof(char));
+	char *rdm_str = malloc(TEST_STR_SIZE*sizeof(char)+1);
 	int next_space = 3+(rand()%6);
-	for(int i = 0; i < TEST_SIZE_CHAR; i++){
+	for(int i = 0; i < TEST_STR_SIZE; i++){
 		switch(rand() % gen_num){
 		case 0:
 			// -1 the size, or '\0' will have a chance of appearing.
@@ -199,9 +208,7 @@ char * generate_random_string(int gen_num){
 		case 1:
 			*(rdm_str+i) = number_array[rand() %					\
 										(sizeof(number_array)-1)];
-			/*			if(*(rdm_str+i) == 0){
-				printw("it equals to b0");
-				}*/
+
 			break;
 		case 2:
 			*(rdm_str+i) = symbol_array[rand() % \
@@ -215,12 +222,11 @@ char * generate_random_string(int gen_num){
 			i++;
 		}
 	}
-	*(rdm_str+TEST_SIZE_CHAR) = '\0';
+	*(rdm_str+TEST_STR_SIZE) = '\0';
 	return rdm_str;
 }
 
 void print_test_string(struct window *op_window, char * str){
-	// BUG: cursor doesn't change with `waddch` after `wmove`
 	mvwaddch(op_window->window_p, 4, 0, *(str));
 	int i = 1;
 	while(*(str+i) != '\0'){
@@ -237,24 +243,24 @@ void show_window(struct window *op_window){
 }
 
 
-void redraw_char(struct window *op_window, const char *test_char,	\
+void redraw_char(struct window *op_window, const char test_char,	\
 				 const char in_correct){
 	switch(in_correct){
 	case 0:
-		waddch(op_window->window_p, *test_char | CORRECT_L);
-		break;
-	case 1:
-		if(*test_char == ' '){
-			waddch(op_window->window_p, *test_char | WRONG_HL);
+		if(test_char == ' '){
+			waddch(op_window->window_p, test_char | WRONG_HL);
 		}else{
-			waddch(op_window->window_p, *test_char | WRONG_L);
+			waddch(op_window->window_p, test_char | WRONG_L);
 		}
 		break;
+	case 1:
+		waddch(op_window->window_p, test_char | CORRECT_L);
+		break;
 	case 2:
-		if(*test_char == ' '){
-			waddch(op_window->window_p, *test_char | REWRITE_HL);		
+		if(test_char == ' '){
+			waddch(op_window->window_p, test_char | REWRITE_HL);		
 		}else{
-			waddch(op_window->window_p, *test_char | REWRITE_L);
+			waddch(op_window->window_p, test_char | REWRITE_L);
 		}
 		break;
 	default:
@@ -266,77 +272,152 @@ void redraw_char(struct window *op_window, const char *test_char,	\
 	}
 }
 
-// should return the timing array for analysis
-void test_string(char *str){
+void backspace_character(struct window *op_window, char rechar){
+	uint8_t y, x;
+	// Macro that determines the position of cursor
+	getyx(op_window->window_p, y, x);
+	if(x == 0){
+		if(y != op_window->start_curs_y){
+			wmove(op_window->window_p, y-1, op_window->length-1);
+			redraw_char(op_window, rechar, 2);
+			wmove(op_window->window_p, y-1, op_window->length-1);
+		}
+	}else{
+		wmove(op_window->window_p, y, x-1);
+		redraw_char(op_window, rechar, 2);
+		wmove(op_window->window_p, y, x-1);
+	}
+}
+
+
+void test_string(struct keystat *instat){
+	// Set all backspace to 0
+	for(int i = 0; i < sizeof(instat->bs); i++){
+		instat->bs[i] = 0;
+	}
+	
 	// Configure the window
 	struct window typing_window;
 	configure_window(&typing_window);
 	print_typing_string(&typing_window);
-	print_test_string(&typing_window, str);
+	print_test_string(&typing_window, instat->test_str);
 	show_window(&typing_window);
 	wmove(typing_window.window_p, 4, 0);
 
+	// Read the first character and initialize timings
+	char char_in = getch();
+	instat->char_input[0] = instat->entered_str[0] = char_in;
+	redraw_char(&typing_window, *instat->test_str, (char_in == *instat->test_str));
+	// Everything to do with timing keypresses
+	struct timeval now_tv;
+	gettimeofday(&now_tv, 0);
+	uint64_t st_t = (now_tv.tv_sec*1000) + (now_tv.tv_usec/1000);
+	instat->timings[0] = 0;
+
+	wrefresh(typing_window.window_p);
+
 	// Start testing keystrokes until we have reached
 	// the string's terminator char
-	int i = 0;
-	int e = 0;
-	while(*(str+i) != '\0'){
-		int char_in = getch();
-		// Backspace
-		if(char_in == 263){
-			int y, x;
-			// Macro that determines the position of cursor
-			getyx(typing_window.window_p, y, x);
-			if(x == 0){
-				if(y != typing_window.start_curs_y){
-					i--;
-					wmove(typing_window.window_p, y-1, typing_window.length-1);
-					redraw_char(&typing_window, str+i, 2);
-					wmove(typing_window.window_p, y-1, typing_window.length-1);
-				}
-			}else{
-				i--;
-				wmove(typing_window.window_p, y, x-1);
-				redraw_char(&typing_window, str+i, 2);
-				wmove(typing_window.window_p, y, x-1);
-			}
-		}else if(char_in == *(str+i)){
-			redraw_char(&typing_window, str+i, 0);
-			i++;
+	uint16_t i = 1,
+		entr_i = 1;
+
+	while(*(instat->test_str+i) != '\0'){
+		char_in = getch();
+		
+		// Backspace (emacs keeps changing which char it wants to use)
+		if(char_in == 263 || char_in == 127 || char_in == 8 || char_in == 7){
+			i--;
+			backspace_character(&typing_window, *(instat->test_str+i));
+
+			// Don't mark as bs if correct initially
+			if(instat->char_input[i] != *(instat->test_str+i)){ 
+				instat->bs[i]++; 
+			} 
 		}else{
-			redraw_char(&typing_window, str+i, 1);
+			redraw_char(&typing_window, *(instat->test_str+i),	\
+						(char_in == *(instat->test_str+i)));
+
+			// Get current time and record it
+			gettimeofday(&now_tv, 0);
+			instat->timings[i] = (now_tv.tv_sec*1000 + now_tv.tv_usec/1000) - st_t;
 			i++;
 		}
+		// Character entry book keeping
+		instat->char_input[i] = char_in;
+		instat->entered_str[entr_i] = char_in;
+		entr_i++; 
+
 		wrefresh(typing_window.window_p);
 	}
 }
-				
+int getavgms(struct keystat *stats){
+	uint64_t sum_t = 0;
+	for(int i = 1; i < stats->test_str_size; i++){
+		sum_t += stats->timings[i] - stats->timings[i-1];
+	}
+	return sum_t/stats->test_str_size;
+}
+
+void
+print_results(struct window *op_window, int avg_t, int wpm, int acc, \
+			  int mstks, int mstks_fx){
+	const char line0[] = "Test finished!";
+
+	WINDOW *win = op_window->window_p;
+	int win_l = op_window->length;
+
+	mvwprintw(win, 0, (win_l - strlen(line0))/2, "%s", line0);
+	mvwprintw(win, 1, 0, "Average keypress: %10dms", avg_t);
+	mvwprintw(win, 2, 0, "Words per minute: %10d", wpm);
+	mvwprintw(win, 3, 0, "Accuracy:         %10d\%", acc);
+	mvwprintw(win, 4, 0, "Mistakes total:   %10d", mstks);
+	mvwprintw(win, 5, 0, "Mistakes fixed:   %10d", mstks_fx);
+	
+	show_window(op_window);
+}
+void display_results(struct keystat *stats){
+	struct window results_window;
+	configure_window(&results_window);
+
+	int avg_t = getavgms(stats);
+	// TODO
+	int wpm = 0,
+		acc = 0,
+		mstks = 0,
+		mstks_fxd = 0;
+
+	print_results(&results_window,				\
+				  avg_t, wpm, acc, mstks, mstks_fxd);
+	
+
+
+
+	getchar();
+}
 int main(){
 	start_curses();
 
 	struct window welcome_menu;
 	configure_window(&welcome_menu);
 
-	// Start of what the typist interacts with 
-	// Quits when 'q' is pressed (char 65)
+	// Start of what the user interacts with 
 	int option_slct = 0;
-	while(option_slct != 65){
+	while(option_slct != 'q'){
 		// Show window, take input
 		print_option_menu(&welcome_menu);
 		show_window(&welcome_menu);
 		option_slct = getch() - '0';
 
-		// Makes sure correct number is entered
-		while((option_slct > 3 ||				\
-			   option_slct < 1) &&				\
-			  option_slct != 65){
-			option_slct = getch() - '0';
-		}
+		// This is the structure that most operations are performed on
+		struct keystat curstat;
 
+		// If first [1-3] selected
 		if(option_slct > 0 && option_slct < 4){
-			char *test_str = generate_random_string(option_slct);
+			curstat.test_str = generate_random_string(option_slct);
+			curstat.test_str_size = TEST_STR_SIZE;
 			werase(welcome_menu.window_p);
-			test_string(test_str);
+			test_string(&curstat);
+			display_results(&curstat);
 		}
 	}
 	exit_application(0);
